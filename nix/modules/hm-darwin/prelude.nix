c921b9f961cadf5c0bc5fa91286c0a2b4d710fd8
ttak0422 <ttak0422@gmail.com>
2024-10-19 14:54:44 +0900

    style: remove unused statements

--- nix/modules/hm-darwin/prelude.nix
+++ nix/modules/hm-darwin/prelude.nix
@@ -9,13 +9,6 @@
 }:
 {
   home = lib.mkIf pkgs.stdenv.isDarwin {
-    # https://texwiki.texjp.org/?Mac#defaultkeybinding-dict
-    file."/Library/KeyBindings/DefaultKeyBinding.dict".text = ''
-      {
-        "¥" = ("insertText:", "\\");
-        "~\\" = ("insertText:", "¥");
-      }
-    '';
     activation.aliasApplications =
       let
         hmApps = pkgs.buildEnv {
