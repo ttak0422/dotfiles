d920e7f40f2e3dec965704af06ad0be1ed5347e3
ttak0422 <ttak0422@gmail.com>
2025-02-11 18:11:52 +0900

    chore(hyprland): add keymaps

--- nix/modules/hm-linux/hyprland/keymap.nix
+++ nix/modules/hm-linux/hyprland/keymap.nix
@@ -1,11 +1,39 @@
-_:
-{
+_: {
   wayland.windowManager.hyprland.settings = {
     "$mainMod" = "SUPER";
     "$subMod" = "ALT";
     "$term" = "wezterm";
     bind = [
       "$mainMod, Return, exec, $term"
+
+      "$subMod, h, exec, movefocus l"
+      "$subMod, j, exec, movefocus d"
+      "$subMod, k, exec, movefocus u"
+      "$subMod, l, exec, movefocus r"
+      "$subMod, z, exec, fullscreen"
+      "$subMod, f, exec, togglefloating"
+
+      "$mainMod, 1, workspace, 1"
+      "$mainMod, 2, workspace, 2"
+      "$mainMod, 3, workspace, 3"
+      "$mainMod, 4, workspace, 4"
+      "$mainMod, 5, workspace, 5"
+      "$mainMod, 6, workspace, 6"
+      "$mainMod, 7, workspace, 7"
+      "$mainMod, 8, workspace, 8"
+      "$mainMod, 9, workspace, 9"
+      "$mainMod, 0, workspace, 10"
+
+      "$mainMod SHIFT, 1, movetoworkspace, 1"
+      "$mainMod SHIFT, 2, movetoworkspace, 2"
+      "$mainMod SHIFT, 3, movetoworkspace, 3"
+      "$mainMod SHIFT, 4, movetoworkspace, 4"
+      "$mainMod SHIFT, 5, movetoworkspace, 5"
+      "$mainMod SHIFT, 6, movetoworkspace, 6"
+      "$mainMod SHIFT, 7, movetoworkspace, 7"
+      "$mainMod SHIFT, 8, movetoworkspace, 8"
+      "$mainMod SHIFT, 9, movetoworkspace, 9"
+      "$mainMod SHIFT, 0, movetoworkspace, 10"
     ];
   };
 }
