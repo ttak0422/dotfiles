7642546f35273c889251257f6837352baa6b9240
ttak0422 <ttak0422@gmail.com>
2025-02-11 11:51:17 +0900

    chore(nix): zen

--- nix/hosts/linux-laptop/default.nix
+++ nix/hosts/linux-laptop/default.nix
@@ -49,9 +49,11 @@
                 common-pc-laptop-ssd
               ]);
 
-            # Bootloader.
-            boot.loader.systemd-boot.enable = true;
-            boot.loader.efi.canTouchEfiVariables = true;
+            boot = {
+              kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+              loader.systemd-boot.enable = true;
+              loader.efi.canTouchEfiVariables = true;
+            };
 
             networking.hostName = "nixos"; # Define your hostname.
             # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
