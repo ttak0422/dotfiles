09eff6bbd7571e15c83cc20ca281273800aa9b6d
ttak0422 <ttak0422@gmail.com>
2025-02-14 00:46:29 +0900

    chore(hyprland): modify deps

--- nix/modules/linux/hyprland/default.nix
+++ nix/modules/linux/hyprland/default.nix
@@ -1,10 +1,20 @@
-_: {
+{ pkgs, ... }:
+{
   imports = [ ./greetd.nix ];
   programs = {
     hyprland.enable = true;
     hyprlock.enable = true;
   };
+  environment.systemPackages = with pkgs; [
+    bluez
+    bluez-tools
+    blueman
+    libgtop
+    dart-sass
+    wl-clipboard
+  ];
   services = {
     upower.enable = true;
+    gvfs.enable = true;
   };
 }
