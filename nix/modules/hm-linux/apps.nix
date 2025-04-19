1fedc66c87f1ea5a7e797566deb8eec0c35c1ec1
ttak0422 <ttak0422@gmail.com>
2025-04-19 20:11:15 +0900

    wip

--- nix/modules/hm-linux/apps.nix
+++ nix/modules/hm-linux/apps.nix
@@ -1,14 +1,18 @@
 { inputs', pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    discord
-    ghostty
-    inputs'.zen-browser.packages.default
-    jetbrains-toolbox
-    neovide
-    slack
-    spotify
-    vscode
-  ];
+  home.packages =
+    with pkgs;
+    [
+      discord
+      ghostty
+      inputs'.zen-browser.packages.default
+      jetbrains-toolbox
+      neovide
+      slack
+      spotify
+    ]
+    ++ (with pkgs-unstable; [
+      vscode
+    ]);
   programs.google-chrome.enable = true;
 }
