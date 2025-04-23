cef7157c6e2a4a0dd2feaf92ff49bb3adc575463
ttak0422 <ttak0422@gmail.com>
2025-04-24 00:19:54 +0900

    chroe(zellij): modify status

--- nix/modules/hm/shell/zellij/default.nix
+++ nix/modules/hm/shell/zellij/default.nix
@@ -11,8 +11,8 @@
                 plugin location="file:${pkgs.zjstatus}/bin/zjstatus.wasm" {
                     format_left   "{tabs}"
                     format_center ""
-                    format_right  "{mode}#[fg=#767b82,bg=#2a2a2e,bold]({session})"
-                    format_space  "[fg=#b5d2a9,bg=#2a2a2e]"
+                    format_right  "{mode}#[fg=#767b82bold]({session})"
+                    format_space  "[fg=#b5d2a9]"
                     first_start "true"
 
                     border_enabled  "false"
@@ -22,14 +22,14 @@
 
                     hide_frame_for_single_pane "false"
 
-                    mode_tmux          "#[fg=#d3c785,bg=#2a2a2e,bold,italic] Zellij "
-                    mode_normal        "#[fg=#767b82,bg=#2a2a2e,bold] Zellij "
-                    mode_scroll        "#[fg=#767b82,bg=#2a2a2e,bold] {name} "
-                    mode_enter_search  "#[fg=#767b82,bg=#2a2a2e,bold] {name} "
-                    mode_search        "#[fg=#767b82,bg=#2a2a2e,bold] {name} "
+                    mode_tmux          "#[fg=#d3c785,bold,italic] Zellij "
+                    mode_normal        "#[fg=#767b82,bold] Zellij "
+                    mode_scroll        "#[fg=#767b82,bold] {name} "
+                    mode_enter_search  "#[fg=#767b82,bold] {name} "
+                    mode_search        "#[fg=#767b82,bold] {name} "
 
-                    tab_normal   " #[fg=#767b82,bg=#2a2a2e]{index}:{name}"
-                    tab_active   " #[fg=#d99872,bg=#2a2a2e,bold]{index}:{name}"
+                    tab_normal   " #[fg=#767b82]{index}:{name}"
+                    tab_active   " #[fg=#d99872,bold]{index}:{name}"
                 }
             }
         }
