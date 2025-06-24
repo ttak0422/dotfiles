a2e5e6beefca13ea05678b4207b116117b79a23b
ttak0422 <ttak0422@gmail.com>
2025-06-24 21:24:40 +0900

    chore(node): add npmrc

--- nix/modules/hm/dev/node.nix
+++ nix/modules/hm/dev/node.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    nodejs_22
-  ];
+  home = {
+    packages = with pkgs; [
+      nodejs_22
+    ];
+    file.".npmrc".text = ''
+      prefix = $HOME/.npm-packages
+    '';
+  };
 }
