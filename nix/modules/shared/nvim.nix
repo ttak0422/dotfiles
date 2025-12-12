673713f7d3c47015a056005ba2f846922b91a6f1
ttak0422 <ttak0422@gmail.com>
2025-12-13 00:29:44 +0900

    refactor: use writeShellScriptBin

--- nix/modules/shared/nvim.nix
+++ nix/modules/shared/nvim.nix
@@ -1,9 +1,8 @@
 { inputs', pkgs, ... }:
 {
   environment.systemPackages = [
-    (pkgs.runCommand "nvim" { } ''
-      mkdir -p $out/bin
-      ln -s ${inputs'.dotfiles-nvim.packages.bundler-nvim-v2}/bin/nvim $out/bin/nvim
+    (pkgs.writeShellScriptBin "nvim" ''
+      exec ${inputs'.dotfiles-nvim.packages.bundler-nvim-v2}/bin/nvim "$@"
     '')
   ];
 }
